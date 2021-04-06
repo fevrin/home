21e9abe5daa6d57d1ee9a400f570c037f67bbf9d
Chrissy Weeks <fevrin@gmail.com>
2021-04-06 02:39:18 -0400

    .gitconfig * added `dffs` and `diff-with-master` aliases
    .gitconfig.d/git-sync.sh
    * removed old code in favor of more concise version

--- .gitconfig.d/git-sync.sh
+++ .gitconfig.d/git-sync.sh
@@ -3,13 +3,10 @@
 export SSH_AUTH_SOCK=/run/user/$(id -u)/keyring/ssh
 
 declare -a DIRS
-STASH="1"
 
 for arg in $@; do
    if [[ -d "$arg" ]]; then
       DIRS+=("$arg")
-   elif [[ "$arg" =~ ^(-n|--no-stash)$ ]]; then
-      STASH="0"
    fi
 done
 
@@ -41,62 +38,25 @@ for DIR in ${DIRS[*]}; do
 
    echo "WORKING ON '$DIR'"
    echo
-#   output=$(git status --untracked-files=no --porcelain)
-#   if [[ "$output" ]]; then
-#      $GITBIN stash push 
-#   else
 
    THERE_WERE_LOCAL_CHANGES="$($GITBIN status --untracked-files=no --porcelain)"
-   TEMP_ID="$(mktemp -u XXXXXXXXXX) @ $(date +%s)"
    GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
-   if [[ $THERE_WERE_LOCAL_CHANGES && $STASH = 1 ]]; then
-      echo "STASHING CURRENT INDEX AND UNSTAGED FILES IN BRANCH '$GIT_BRANCH'"
-
-      # stashing doesn't retain files' index status, so we have to track that
-      # nvm, `git stash pop --index` restores the index status
-#      STAGED_FILES="$($GITBIN diff --cached --name-only)"
-
-      STASH_MSG="git sync temp stash $TEMP_ID"
-      $GITBIN stash push -m "$STASH_MSG"
-      STASH_REF="$(git stash list | grep "$STASH_MSG" | cut -d: -f1)"
-      echo
-   fi
-
    echo "UPDATING MASTER..."
-   [[ $GIT_BRANCH =~ ^master$ ]] || $GITBIN checkout master
    ORIGINAL_MASTER_REF="$(git rev-parse master)"
-   $GITBIN pull --ff-only
+   $GITBIN fetch origin master:master
    echo
 
-   # if the original branch wasn't master, and updates were made to master,
-   # rebase it from master
-   if [[ ! $GIT_BRANCH =~ ^master$ ]]; then
-      $GITBIN checkout -
+   echo "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' ONTO MASTER"
+   $GITBIN rebase --autostash master || {
+      [[ -f .git/rebase-merge/done ]] &&
+         echo "ABORTING GIT REBASE '$GIT_BRANCH' ONTO MASTER" &&
+         $GITBIN rebase --abort
+         date "+%F %H:%M:%S %Z: failure" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
+   }
+   echo
 
-#      if [[ $ORIGINAL_MASTER_REF = $(git rev-parse master) ]]; then
-#         echo "MASTER UNCHANGED; SKIPPING REBASE OF '$GIT_BRANCH' FROM MASTER"
-#      else
-         echo "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' FROM MASTER"
-         $GITBIN rebase master || {
-            [[ -f .git/rebase-merge/done ]] &&
-               echo "ABORTING GIT REBASE '$GIT_BRANCH' FROM MASTER" &&
-               $GITBIN rebase --abort
-         }
-         echo
-#      fi
-   fi
-
-   if [[ $THERE_WERE_LOCAL_CHANGES && $STASH = 1 ]]; then
-      echo "POPPING GIT STASH"
-      $GITBIN stash pop --index "$STASH_REF"
-
-      # restore index status of the files
-#      [[ $STAGED_FILES ]] && $GITBIN add $STAGED_FILES
-   fi
-#   fi
-
-   date "+%F %H:%M:%S %Z" | tee -a $HOME/${DIR##*/}-git-pull-timestamps
+   date "+%F %H:%M:%S %Z: success" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
 
    echo
 
