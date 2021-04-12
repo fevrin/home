3adeb8343046847197084429beedb6144cc983e3
Chrissy Weeks <fevrin@gmail.com>
2021-04-12 15:02:22 -0400

    .gitconfig * updated aliases to be more concise
    .gitconfig.d/git-sync.sh
    * stash any staged files and re-add them after rebasing

--- .gitconfig.d/git-sync.sh
+++ .gitconfig.d/git-sync.sh
@@ -7,6 +7,8 @@ declare -a DIRS
 for arg in $@; do
    if [[ -d "$arg" ]]; then
       DIRS+=("$arg")
+   elif [[ "$arg" =~ -a( |$) ]]; then
+      GIT_ADD_FILES=1
    fi
 done
 
@@ -37,23 +39,51 @@ for DIR in ${DIRS[*]}; do
    }
 
    echo "WORKING ON '$DIR'"
-   echo
+
+   [[ $GIT_ADD_FILES -eq 1 ]] && {
+      echo
+      echo
+      echo "STAGING FILES..."
+      $GITBIN add -u
+   }
 
    THERE_WERE_LOCAL_CHANGES="$($GITBIN status --untracked-files=no --porcelain)"
+   STAGED_FILES="$(git diff --staged --name-only)"
    GIT_BRANCH="$(git branch --show-current)"
 
+   echo
+   echo
    echo "UPDATING MASTER..."
    ORIGINAL_MASTER_REF="$(git rev-parse master)"
-   $GITBIN fetch origin master:master
+   if [[ "$GIT_BRANCH" =~ master ]]; then
+      $GITBIN pull
+   else
+      $GITBIN pull origin master:master 
+   fi
    echo
 
+   if [[ -n "$STAGED_FILES" ]]; then
+      echo
+      echo "THESE STAGED FILES WILL BE STASHED AND RE-STAGED:"
+      echo "$STAGED_FILES"
+   fi
+
+
+   echo
+   echo
    echo "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' ONTO MASTER"
    $GITBIN rebase --autostash master || {
       [[ -f .git/rebase-merge/done ]] &&
+         echo &&
+         echo &&
          echo "ABORTING GIT REBASE '$GIT_BRANCH' ONTO MASTER" &&
          $GITBIN rebase --abort
          date "+%F %H:%M:%S %Z: failure" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
    }
+
+   echo
+   echo
+   [[ -n "$STAGED_FILES" ]] && echo "RE-ADDING STAGED FILES" && git add --ignore-errors $STAGED_FILES
    echo
 
    date "+%F %H:%M:%S %Z: success" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
