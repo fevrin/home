72922fba53a6361bb45146783defe1569acf6b55
Chrissy Weeks <fevrin@gmail.com>
2021-06-08 00:56:14 -0400

    .bashrc.d/base * switched back to `tee` from `sponge` * added `mtr` to HISTIGNORE
    .bashrc.d/functions_sec
    * added `ssh_key_is_encrypted()`
    
    .bashrc.d/remote/remote_base
    * fixed source command to ensure it isn't going to recursively source
    
    .gitconfig
    * updated various aliases to support `main` branch
    * added aliases to detect the main branch and main remote names
    
    .gitconfig.d/git-sync.sh
    * added support for `main` branch name
    * added some checks and verbosity
    
    etc/hosts
    * added more domains to blacklist

--- .gitconfig.d/git-sync.sh
+++ .gitconfig.d/git-sync.sh
@@ -29,7 +29,7 @@ done
 GITBIN="/usr/bin/git"
 
 for DIR in ${DIRS[*]}; do
-   cd $DIR
+   cd $(git rev-parse --show-toplevel)
 
    # check if the directory is a git repo
    git rev-parse --is-inside-work-tree &>/dev/null || {
@@ -40,7 +40,7 @@ for DIR in ${DIRS[*]}; do
 
    echo "WORKING ON '$DIR'"
 
-   [[ $GIT_ADD_FILES -eq 1 ]] && {
+   [[ $GIT_ADD_FILES -eq 1 && $(git diff --quiet --exit-code; echo $?) -eq 1 ]] && {
       echo
       echo
       echo "STAGING FILES..."
@@ -53,12 +53,21 @@ for DIR in ${DIRS[*]}; do
 
    echo
    echo
-   echo "UPDATING MASTER..."
-   ORIGINAL_MASTER_REF="$(git rev-parse master)"
-   if [[ "$GIT_BRANCH" =~ master ]]; then
+   echo "UPDATING MAIN BRANCH..."
+   MAIN_BRANCH_NAME="master"
+   git rev-parse --verify --quiet $MAIN_BRANCH_NAME || MAIN_BRANCH_NAME="main"
+   ORIGINAL_MAIN_REF="$(git rev-parse $MAIN_BRANCH_NAME)"
+   if [[ "$GIT_BRANCH" =~ $MAIN_BRANCH_NAME ]]; then
+      echo "$GITBIN pull"
       $GITBIN pull
    else
-      $GITBIN pull origin master:master 
+      echo "$GITBIN fetch origin $MAIN_BRANCH_NAME:$MAIN_BRANCH_NAME"
+      $GITBIN fetch origin $MAIN_BRANCH_NAME:$MAIN_BRANCH_NAME
+   fi
+
+   if [[ $? -ne 0 ]]; then
+      echo "GIT PULL FAILED! ABORTING FURTHER CHANGES"
+      continue
    fi
    echo
 
@@ -69,22 +78,26 @@ for DIR in ${DIRS[*]}; do
    fi
 
 
-   echo
-   echo
-   echo "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' ONTO MASTER"
-   $GITBIN rebase --autostash master || {
-      [[ -f .git/rebase-merge/done ]] &&
-         echo &&
-         echo &&
-         echo "ABORTING GIT REBASE '$GIT_BRANCH' ONTO MASTER" &&
-         $GITBIN rebase --abort
-         date "+%F %H:%M:%S %Z: failure" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
-   }
+   if [[ ! "$GIT_BRANCH" =~ $MAIN_BRANCH_NAME ]]; then
+      echo
+      echo
+      echo "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' ONTO '$MAIN_BRANCH_NAME'"
+      $GITBIN rebase --autostash $MAIN_BRANCH_NAME || {
+         [[ -f .git/rebase-merge/done ]] &&
+            echo &&
+            echo &&
+            echo "ABORTING GIT REBASE '$GIT_BRANCH' ONTO '$MAIN_BRANCH_NAME'" &&
+            $GITBIN rebase --abort
+            date "+%F %H:%M:%S %Z: failure" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
+      }
+   fi
 
-   echo
-   echo
-   [[ -n "$STAGED_FILES" ]] && echo "RE-ADDING STAGED FILES" && git add --ignore-errors $STAGED_FILES
-   echo
+   [[ -n "$STAGED_FILES" ]] && {
+      echo
+      echo
+      echo "RE-ADDING STAGED FILES" && git add --ignore-errors $STAGED_FILES
+      echo
+   }
 
    date "+%F %H:%M:%S %Z: success" | tee -a $HOME/$(basename "$DIR")-git-pull-timestamps
 
