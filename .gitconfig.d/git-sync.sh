5029365a57b320bf05451d27273f88e244e48eec
Chrissy Weeks <fevrin@gmail.com>
2022-04-12 20:42:07 -0400

    .bashrc.d/functions/_find_unscoped_vars * fixed a grep's regex
    .bashrc.d/functions/_print_var_vals
    * added the ability to pass certain flags on to `verbose`
    
    .bashrc.d/functions/_verify_reqs
    * fixed a variable's scope
    * updated some output strings
    
    .bashrc.d/functions/pushd
    * removed default verbosity
    
    .gitconfig
    * updated `git bclean` to not rebase, just fetch
    * updated `diff-with` aliases to correctly support arbitrary arguments
    
    .gitconfig.d/git-sync.sh
    * reorganized code
    * added more logging
    * added conditionals to account for more cases

--- .gitconfig.d/git-sync.sh
+++ .gitconfig.d/git-sync.sh
@@ -2,30 +2,43 @@
 
 # `git pull`s the main branch of the provided repos and rebases the current branch on that
 
+# TODO: add color support when INTERACTIVE = 1
+# e.g., `git -c color.advice.hint=yellow rebase main`
+# `git -c color.status=always status`
+# you can get the custom color for a given item, if any, using something like:
+# `git config --get-color color.advice`
+
 export SSH_AUTH_SOCK=/run/user/$(id -u)/keyring/ssh
 
+# we have to source these since this is a script and not an environmental function
 . $HOME/.bashrc.d/functions/verbose
 . $HOME/.bashrc.d/functions/_print_var_vals
 . $HOME/.bashrc.d/functions/_verify_reqs
-VERBOSE=8
+#VERBOSE=8
 INTERACTIVE=$(! tty -s; echo $?) # 1 if interactive; 0 if not
+REBASE=1
 
 declare -a DIRS
 
 for arg in $@; do
    if [[ -d "$arg" ]]; then
       DIRS+=("$arg")
-   elif [[ "$arg" =~ -a( |$) ]]; then
+   elif [[ "$arg" =~ ^-a( |$) ]]; then
       GIT_ADD_FILES=1
+   elif [[ "$arg" =~ ^--no-rebase( |$) ]]; then
+      REBASE=0
    fi
 done
 
 [[ ${#DIRS[*]} -gt 0 ]] || DIRS+=("$PWD")
 
 log() {
-   local message="$@"
-#   _print_var_vals DIR
-   date "+%F %H:%M:%S %Z: ($$) $(basename "$REPO_ROOT"): $(verbose 8 $message 2>&1)" | tee -a $HOME/$(basename "$REPO_ROOT")-git-pull-timestamps
+   local messages="$@"
+   local IFS=$'\n'
+   for message in $messages; do
+   [[ "$INTERACTIVE" -eq 1 ]] && echo -e "$message" 2>&1
+   date "+%F %H:%M:%S %Z: ($$) $(basename "$REPO_ROOT"): $(echo -e "$message" 2>&1)" >> $HOME/$(basename "$REPO_ROOT")-git-pull-timestamps
+done
 }
 
 #echo "DIRS = '"${DIRS[*]}"'"
@@ -43,59 +56,23 @@ log() {
 GITBIN="/usr/bin/git"
 
 for DIR in ${DIRS[*]}; do
-   log "INTERACTIVE = '$INTERACTIVE'"
    cd "$DIR"
    REPO_ROOT="$(git rev-parse --show-toplevel)"
    cd "$REPO_ROOT"
 
    # check if the directory is a git repo
-   git rev-parse --is-inside-work-tree &>/dev/null || {
+   $GITBIN rev-parse --is-inside-work-tree &>/dev/null || {
       log "not a git directory: '$DIR'"
       log "skipping!"
       continue
    }
 
-   echo "WORKING ON '$DIR'"
+#   log "INTERACTIVE = '$INTERACTIVE'"
 
-   [[ $GIT_ADD_FILES -eq 1 && $(git diff --quiet --exit-code; echo $?) -eq 1 ]] && {
-      echo
-      echo
-      echo "STAGING FILES..."
-      $GITBIN add -u
-   }
-
-   THERE_WERE_LOCAL_CHANGES="$($GITBIN status --untracked-files=no --porcelain)"
-   STAGED_FILES="$(git diff --staged --name-only)"
-   UNSTAGED_FILES="$(git diff --name-only)"
-   UNMERGED_FILES="$(git status --porcelain=v2 | sed -rne 's;^u.? .* ([^ ]+)$;\1;p')"
-   GIT_BRANCH="$(git branch --show-current)"
-
-   if [[ -n "$UNMERGED_FILES" ]]; then
-      echo
-      log "THERE ARE UNMERGED FILES; EXITING..."
-      log "$UNMERGED_FILES"
-      if [[ "$INTERACTIVE" -eq 1 ]]; then
-         $GITBIN status
-      else
-         log "$($GITBIN status 2>&1)"
-      fi
-      exit
-   fi
-
-   for status in STAGED_FILES UNSTAGED_FILES; do
-      if [[ -n "${!status}" ]]; then
-         echo
-         echo "THESE ${status/_FILES/} FILES WILL BE STASHED AND RE-STAGED:"
-         echo "${!status}"
-         echo
-         STASH_NAME="git-sync.$$"
-      fi
-   done
-   [[ -n "$STASH_NAME" ]] && $GITBIN stash push --message "$STASH_NAME"
+   log "WORKING ON '$DIR'"
 
    echo
-   echo
-   echo "UPDATING MAIN BRANCH..."
+   log "UPDATING MAIN BRANCH..."
    MAIN_BRANCH="$(git get-main-branch)"
    MAIN_REMOTE="$(git get-main-remote)"
 #   git rev-parse --verify --quiet $MAIN_BRANCH || MAIN_BRANCH="main"
@@ -105,7 +82,54 @@ for DIR in ${DIRS[*]}; do
    UPSTREAM="upstream"
 #   [[ -n $(git branch --list $UPSTREAM) ]] && git ls-remote --heads $UPSTREAM &>/dev/null {
    MERGE_TYPE=
-   git fetch --all --prune
+   fetch_output=$($GITBIN fetch --all --prune 2>&1; exit $?)
+   fetch_exit_code="$?"
+   log "$fetch_output"
+   [[ $fetch_exit_code -eq 0 ]] || {
+      log "failure"
+      continue
+   }
+
+   if [[ "$REBASE" -eq 1 ]]; then
+      [[ $GIT_ADD_FILES -eq 1 && $(git diff --quiet --exit-code; echo $?) -eq 1 ]] && {
+         echo
+         echo
+         log "STAGING FILES..."
+         $GITBIN add -u
+      }
+
+      THERE_WERE_LOCAL_CHANGES="$($GITBIN status --untracked-files=no --porcelain)"
+      STAGED_FILES="$(git diff --staged --name-only)"
+      UNSTAGED_FILES="$(git diff --name-only)"
+      UNMERGED_FILES="$(git status --porcelain=v2 | sed -rne 's;^u.? .* ([^ ]+)$;\1;p')"
+      GIT_BRANCH="$(git branch --show-current)"
+
+      if [[ -n "$UNMERGED_FILES" ]]; then
+         echo
+         log "THERE ARE UNMERGED FILES; EXITING..."
+         log "$UNMERGED_FILES"
+         if [[ "$INTERACTIVE" -eq 1 ]]; then
+            $GITBIN status
+         else
+            log "$($GITBIN status 2>&1)"
+         fi
+         exit 1
+      fi
+
+      for status in STAGED_FILES UNSTAGED_FILES; do
+         if [[ -n "${!status}" ]]; then
+            echo
+            log "THESE ${status/_FILES/} FILES WILL BE STASHED AND RE-STAGED:"
+            log "${!status}"
+            echo
+            STASH_NAME="git-sync.$$"
+         fi
+      done
+      [[ -n "$STASH_NAME" ]] && $GITBIN stash push --message "$STASH_NAME"
+      echo
+      log "$(_print_var_vals -e STASH_NAME STAGED_FILES UNSTAGED_FILES UNMERGED_FILES 2>&1)"
+   fi
+
    if git ls-remote --heads $UPSTREAM &>/dev/null; then
       # we're in a forked repo (assuming the upstream repo is aliased to $UPSTREAM)
 
@@ -137,6 +161,7 @@ for DIR in ${DIRS[*]}; do
          $GITBIN push $MAIN_REMOTE $MAIN_BRANCH:$MAIN_BRANCH
       fi
    else
+      log "$(_print_var_vals -e GIT_BRANCH MAIN_BRANCH 2>&1)"
       if [[ "$GIT_BRANCH" =~ $MAIN_BRANCH ]]; then
          echo "$GITBIN merge"
          $GITBIN merge || {
@@ -144,6 +169,7 @@ for DIR in ${DIRS[*]}; do
          }
 
       else
+         verbose 8 "$GITBIN fetch $MAIN_REMOTE $MAIN_BRANCH:$MAIN_BRANCH"
          echo "$GITBIN fetch $MAIN_REMOTE $MAIN_BRANCH:$MAIN_BRANCH"
          $GITBIN fetch $MAIN_REMOTE $MAIN_BRANCH:$MAIN_BRANCH || {
             MERGE_TYPE=fetch
@@ -159,55 +185,57 @@ for DIR in ${DIRS[*]}; do
       continue
    fi
 
-   # the main branch is already updated, so we just need to update the current branch if it's not the main one
-   if [[ ! "$GIT_BRANCH" =~ $MAIN_BRANCH ]]; then
-      echo
-      echo
-      echo "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' ONTO '$MAIN_BRANCH'"
-      if [[ "$INTERACTIVE" -eq 1 ]]; then
-         $GITBIN rebase $MAIN_BRANCH
-         rebase_exit_code="$?"
-      else
+   if [[ "$REBASE" -eq 1 ]]; then
+      # the main branch is already updated, so we just need to update the current branch if it's not the main one
+      if [[ ! "$GIT_BRANCH" =~ $MAIN_BRANCH ]]; then
+         echo
+         echo
+         log "ATTEMPTING TO GIT REBASE '$GIT_BRANCH' ONTO '$MAIN_BRANCH'"
          rebase_result=$($GITBIN rebase $MAIN_BRANCH 2>&1; exit $?)
          rebase_exit_code="$?"
+         #echo "rebase_result = '$rebase_result'"
+         #echo "rebase_exit_code = '$rebase_exit_code'"
          log "$rebase_result"
-      fi
-      if [[ $rebase_exit_code -eq 0 ]]; then
-         echo
-         log $(_print_var_vals STASH_NAME STAGED_FILES UNSTAGED_FILES UNMERGED_FILES)
-         echo
-         if [[ -n "$STASH_NAME" ]]; then
-            echo "POPPING STASHED FILES"
-#            verbose 8 $GITBIN stash pop --index stash@{/"$STASH_NAME"}
-            log "$GITBIN stash pop --index stash@{/\"$STASH_NAME\"}"
-            $GITBIN stash pop --index stash@{/"$STASH_NAME"}
-            POP_EXIT_CODE="$?"
-            log "stash pop exit code: '$POP_EXIT_CODE'"
-            [[ "$POP_EXIT_CODE" -ne 0 ]] && exit 1
-         fi
-      else
-         if [[ -f .git/rebase-merge/done ]]; then
+         if [[ $rebase_exit_code -eq 0 ]]; then
             echo
-            echo
-            echo -n "ABORT GIT REBASE '$GIT_BRANCH' ONTO '$MAIN_BRANCH'? [Y/n] "
-            read
-            if [[ ! "$REPLY" =~ ^n$ || $INTERACTIVE -eq 0 ]]; then
-               $GITBIN rebase --abort
-               log "failure: rebase aborted"
-               [[ -n "$STASH_NAME" ]] && log "stash saved as 'stash@{/"$STASH_NAME"}'"
-            else
-               log 'failure: resolve any conflicts to fully rebase this branch, then retry `git-sync`'
+            if [[ -n "$STASH_NAME" ]]; then
+               log "POPPING STASHED FILES"
+#               verbose 8 $GITBIN stash pop --index stash@{/"$STASH_NAME"}
+               log "$GITBIN stash pop --index stash@{/\"$STASH_NAME\"}"
+               if [[ "$INTERACTIVE" -eq 1 ]]; then
+                  $GITBIN stash pop --index stash@{/"$STASH_NAME"}
+               else
+                  pop_result="$($GITBIN stash pop --index stash@{/"$STASH_NAME"})"
+               fi
+               POP_EXIT_CODE="$?"
+               log "$pop_result"
+               log "stash pop exit code: '$POP_EXIT_CODE'"
+               [[ "$POP_EXIT_CODE" -ne 0 ]] && exit 1
             fi
          else
-            log "failure: no rebase to abort"
+            if [[ -f .git/rebase-merge/done ]]; then
+               echo
+               echo
+               echo -n "ABORT GIT REBASE '$GIT_BRANCH' ONTO '$MAIN_BRANCH'? [Y/n] "
+               read
+               if [[ ! "$REPLY" =~ ^n$ || $INTERACTIVE -eq 0 ]]; then
+                  $GITBIN rebase --abort
+                  log "failure: rebase aborted"
+                  [[ -n "$STASH_NAME" ]] && log "stash saved as 'stash@{/"$STASH_NAME"}'"
+               else
+                  log 'failure: resolve any conflicts to fully rebase this branch, then retry `git-sync`'
+               fi
+            else
+               log "failure: no rebase to abort"
+            fi
+            exit 1
          fi
-         exit 1
       fi
+
+      echo
+
+      log "success"
    fi
-
-   echo
-
-   log "success"
 
 #   cd - &>/dev/null
 done
